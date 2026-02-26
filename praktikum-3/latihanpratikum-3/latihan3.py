00a14e4460cdb8e12c34e6f748f9ac47a23ec8d4
muhammadikhsanasshadiqi16-rgb <muhammadikhsanasshadiqi16@gmail.com>
2026-02-26 23:24:32 +0700

    praktikum-3

---
+++ praktikum-3/latihanpratikum-3/latihan3.py
@@ -0,0 +1,27 @@
+# name chararcter skill and excet
+class character:
+    def __init__(self, char, skill, excess):
+        self.char = char 
+        self.skill = skill
+        self.excess = excess
+
+    def introduce_self(self):
+        print(f"char{self.char}, skill{self.skill}, excess{self.excess}")
+
+    def change_char(self, newchar):
+        self.char=newchar
+        print(f"char {self.char}")
+
+c1=character("goro", "1", "bigpunch" )
+c2=character("lapet", "4", "longpunch")
+c3=character("lolok", "6", "flypunch")
+
+c1.introduce_self()
+c2.introduce_self()
+c3.introduce_self()
+
+
+
+
+
+
